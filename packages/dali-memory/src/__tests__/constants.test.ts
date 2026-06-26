54b499e8280c8d08955e23b04158be27ef489dee
Daniel Maricic <daniel@woss.io>
2026-06-26 17:51:56 +0200

    rm old dali-memory

--- packages/dali-memory/src/__tests__/constants.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vite-plus/test';
-import { pluginName } from '../constants.ts';
-
-describe('constants', () => {
-  it('pluginName is dali-memory', () => {
-    expect(pluginName).toBe('dali-memory');
-  });
-});
